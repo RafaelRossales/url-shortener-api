06c3817f3be162aae046e8ef5372a4c1a71b4b3a
Rafael_Rossales <rapha2704@gmail.com>
2024-11-24 20:34:31 -0300

    feat: implement URL services shortUrl and findByShortUrl

--- src/modules/urls/urls.service.ts
+++ src/modules/urls/urls.service.ts
@@ -1,3 +1,34 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUrlDto } from './dto/create-url.dto';
+import { UpdateUrlDto } from './dto/update-url.dto';
+import { UrlEntity } from './entities/url.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../users/entities/user.entity';
+import * as crypto from 'crypto';
 
+@Injectable()
 export class UrlsService {
+
+  constructor(
+    @InjectRepository(UrlEntity)
+    private readonly urlRepository:Repository<UrlEntity>
+  ){}
+
+  async shortUrl(originalUrl: string,user:UserEntity): Promise<UrlEntity> {
+    const shortUrl = crypto.randomBytes(6).toString('base64');
+    const url = this.urlRepository.create({originalUrl,shortUrl,user});
+    return this.urlRepository.save(url);
+  }
+
+  async findByShortUrl(shortUrl: string): Promise<UrlEntity> {
+    const url = await this.urlRepository.findOne({where:{shortUrl,deletedAt: null}});
+    if(!url) throw new NotFoundException('Url not found');
+    url.clicks += 1;
+    await this.urlRepository.save(url);
+    return url;
+  }
+
+
+
 }
